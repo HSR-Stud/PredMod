24c034ab4e0d55bb8ff4c0cffc90b5f10400caa0
David Wright <david.wright@bluewin.ch>
2018-06-09 13:26:42 +0200

    Improved details on interpretation of linear model R output and added prediction/confidence interval functionality to prediction of new data in example.

--- sections/RTutorial/highDegreeModPred.R
+++ sections/RTutorial/highDegreeModPred.R
@@ -13,8 +13,10 @@ plot(a$X2, a$Y2, xlab="X2", ylab="Y2")
 # printing the summary
 print(summary(reg))
 
-# generating a new input data sequence
+# generating a new input data sequence, confidence and prediction intervals can be chosen
 newData = data.frame(X2=seq(4, 14, length=11))
+newData = data.frame(X2=seq(4, 14, length=11), interval="prediction", level=0.95)
+newData = data.frame(X2=seq(4, 14, length=11), interval="confidence", level=0.95)
 
 # predicting the output
 quad <- predict(reg, newData)
